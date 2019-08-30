c9ce0863b0e0a74d9c4c5cbf3a94524d45a3ab52
Gert Wijnalda <gert@redant.nl>
2019-08-30 14:51:39 +0200

    Preserve indentation for serialized input and interface fields after description

--- src/Serializers/TypeSerializers/InterfaceSerializer.php
+++ src/Serializers/TypeSerializers/InterfaceSerializer.php
@@ -40,7 +40,7 @@ class InterfaceSerializer
         /** @var Field $field */
         foreach ($type->getFields()->getIterator() as $field) {
             $string .= '  ';
-            $string .= (new FieldSerializer())->serialize($field);
+            $string .= str_replace("\n", "\n  ", (new FieldSerializer())->serialize($field));
             $string .= "\n";
         }
 
